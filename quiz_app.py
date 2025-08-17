ea96bd514f8d4f47f5009dea11f8cbfc4b072ad7
EglandZeneli <eglandzenelI@gmail.com>
2025-08-17 21:09:07 +0200

    Update quiz_app.py
    cf

--- quiz_app.py
+++ quiz_app.py
@@ -220,7 +220,7 @@ if not st.session_state.finished:
             st.session_state.answered = False
             if st.session_state.current_q >= total:
                 st.session_state.finished = True
-           st.experimental_rerun()
+            st.rerun()
 
 
 # ---------- FINISH ----------
@@ -238,4 +238,4 @@ else:
         st.session_state.current_q = 0
         st.session_state.score = 0
         st.session_state.finished = False
-      st.experimental_rerun()
+        st.rerun()
